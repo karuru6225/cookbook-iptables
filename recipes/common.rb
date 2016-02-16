e49042272cf8a421749f18a59895f464b55b94c1
karuru6225 <e2hjhi@gmail.com>
2016-02-16 13:26:40 +0900

    ディストリ別に、initscriptを切り替える。iptablesのルールファイル生成のバグ修正

--- recipes/common.rb
+++ recipes/common.rb
@@ -5,8 +5,16 @@ directory '/etc/iptables/' do
   mode 00755
 end
 
+iptables_template = ''
+case node["platform_family"]
+when "debian"
+  iptables_template = 'iptables_deb.service'
+when "rhel"
+  iptables_template = 'iptables_rh.service'
+end
+
 cookbook_file '/etc/init.d/iptables' do
-  source 'iptables.service'
+  source iptables_template
   owner 'root'
   group 'root'
   mode 00755
