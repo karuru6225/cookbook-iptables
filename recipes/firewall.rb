3ee5b8a319ad1d89e98234052026dbdfa82549c9
karuru6225 <e2hjhi@gmail.com>
2016-02-19 13:17:16 +0900

    パラメータ渡すの忘れてた

--- recipes/firewall.rb
+++ recipes/firewall.rb
@@ -9,6 +9,7 @@ template '/etc/iptables/iptables' do
   variables(
     lan_net: settings['lan_net'],
     accept_ports: settings['accept_ports'],
+    accept_only_jp: settings['accept_only_jp'],
     drop_countries: settings['drop_countries']
   )
   notifies :restart, 'service[iptables]', :delayed
