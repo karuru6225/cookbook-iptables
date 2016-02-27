898e71b7f714675f47b760d19028ead7284acc82
karuru6225 <e2hjhi@gmail.com>
2016-02-28 01:26:11 +0900

    pushし忘れ

--- recipes/router.rb
+++ recipes/router.rb
@@ -1,31 +1,34 @@
 include_recipe "iptables::common"
 
-settings = node['iptables'] || Chef::EncryptedDataBagItem.load('iptables', 'settings')
+settings = node['iptables'] ||
+           Chef::EncryptedDataBagItem.load('iptables', 'settings')
 template '/etc/iptables/iptables' do
   source 'iptables_router.sh.erb'
   owner 'root'
   group 'root'
   mode 00744
-  variables({
+  variables(
     lan_net: settings['lan_net'],
     forwards: settings['forwards'],
     drop_countries: settings['drop_countries']
-  })
+  )
   notifies :restart, 'service[iptables]', :delayed
 end
 
-package 'linux-igd' do
-  action :install
-end
+if node['iptables']['upnp']
+  package 'linux-igd' do
+    action :install
+  end
 
-template '/etc/default/linux-igd' do
-  source 'linux-igd.erb'
-  owner 'root'
-  group 'root'
-  mode 00644
-  notifies :restart, 'service[linux-igd]', :delayed
-end
+  template '/etc/default/linux-igd' do
+    source 'linux-igd.erb'
+    owner 'root'
+    group 'root'
+    mode 00644
+    notifies :restart, 'service[linux-igd]', :delayed
+  end
 
-service 'linux-igd' do
-  action :enable
+  service 'linux-igd' do
+    action :enable
+  end
 end
